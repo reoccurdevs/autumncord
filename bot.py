7e5dce54b6ce86795a921bc3be88645a2578049c
reoccurcat <reoccurcat@gmail.com>
2021-12-04 18:17:58 -0600

    h

--- bot.py
+++ bot.py
@@ -390,7 +390,7 @@ async def on_message(msg):
                 return
             except asyncio.TimeoutError:
                 return
-        if notacommand is True and msg.author.id != bot.user.id and guildconfig["bumpreminder"] == "True" and str(msg.content).startswith("!d bump "):
+        if notacommand is True and msg.author.id != bot.user.id and guildconfig["bumpreminder"] == "True" and str(msg.content).startswith("!d bump") and msg.content.split("bump"[1]).startswith(" "):
             await asyncio.sleep(0.5)
             messages = await msg.channel.history(limit=2).flatten()
             embeds = messages[1].embeds
